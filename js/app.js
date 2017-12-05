df7da7d5910f87dc8c05dd690eee1893a2b63eee
luge <epitomizelu@163.com>
2017-12-06 00:14:59 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,73 @@
+(function() {
+    /**
+     * 在 map.js 中声明了两个全局变量 ： mapControl（用户与地图的交互接口） 和 aMap(高德地图对象)
+     *
+     * 在 app.js 中声明了一个变量： viewModel
+     */
+
+    mapControl.init('container');
+    mapControl.search(); 
+
+    var ViewModel = function() {
+    	this.pois = ko.observableArray();
+    	this.selectedPoiIndex = ko.observable(0);
+    	this.showOrHidePoiList = ko.observable('block');
+    	this.current = ko.observable('');
+    	this.filterPoisList = ko.observableArray();
+
+    	var self = this;
+
+        // 点击左侧POI 列表，更新左侧列表和右侧地图
+    	this.update = function() {
+    		var obj = this;
+    		var index = self.filterPoisList.indexOf(obj);
+    		self.updateMarker(index);
+    		self.updateAsidePoiListView(index);
+    	}
+        
+        // 更新地图
+    	this.updateMarker = function (index) {  
+    		mapControl.fastMarkChange(index); 
+    	}
+
+        // 更新左侧列表
+    	this.updateAsidePoiListView = function(index) {
+    	    self.selectedPoiIndex(index); 
+    	}
+
+        // 给左侧列表选中项增加选中样式
+    	this.poiSelected = function(index) {
+    		return self.selectedPoiIndex() === index();
+    	}
+
+        // 显示 or 隐藏 左侧的POI信息列表
+    	this.changeDisplayOfPoiList = function() {
+    		var display = this.showOrHidePoiList() === 'block' ? 'none' : 'block';
+    		this.showOrHidePoiList(display);
+    	}
+
+        // 用户按 enter 键 或者 查询按钮时根据用户输入过滤，如果用户输入为空，则显示全部
+    	this.filterPois = function() {
+        	var current = self.current().trim(); 
+
+        	self.filterPoisList.removeAll();
+        	self.pois().forEach(function(item) {
+            	if(item.name.indexOf(current) != -1) {
+            		self.filterPoisList.push(item);
+            	}
+        	});
+
+        	self.recreateMarkers();
+    	}
+
+        //  创建marker
+    	this.recreateMarkers = function() {
+    		mapControl.clearAllMarkers();
+    		mapControl.createMarkers(this.filterPoisList()); 
+    	}
+    }
+
+
+    window.viewModel = new ViewModel();
+	ko.applyBindings(viewModel);
+})();
---
+++ js/map.js
@@ -0,0 +1,145 @@
+(function() {
+    var Map = function() {
+    	this.markers = [];
+    	this.selectedRedMarkerIndex = 0;
+    	this.redMarkerImageUrl =  'http://webapi.amap.com/theme/v1.3/markers/n/mark_r.png';
+    	this.defaultMarkerImageUrl =  'http://webapi.amap.com/theme/v1.3/markers/n/mark_b.png';
+    };
+
+    Map.prototype.init = function(selector) {
+        var aMap = new AMap.Map(selector, {
+            resizeEnable: true
+        });
+        
+        this.selectedRedMarker = this.createIcon('http://webapi.amap.com/theme/v1.3/markers/n/mark_r.png');
+        // 第一个大坑，aMap一定必须是全局变量，而不能是对象属性
+        window.aMap = aMap;
+    }
+
+    // 查找 POI 信息
+    Map.prototype.search = function(code) {
+    	var that = this;
+
+        AMap.service(["AMap.PlaceSearch"], function() {
+            var placeSearch = new AMap.PlaceSearch({ //构造地点查询类
+                pageSize: 5,
+                pageIndex: 1,
+                city: "佛山", //城市
+                map: aMap
+            });
+            // 关键字查询
+            placeSearch.search('新华书店', function(status, result) {
+                console.log(status, result);
+
+                viewModel.pois.removeAll();
+                result.poiList.pois.forEach(function(item) {
+                    viewModel.pois.push(item);
+                });
+                viewModel.filterPois();
+                viewModel.recreateMarkers(); 
+            });
+        });
+    }
+
+    // 清除地图上所有的marker
+    Map.prototype.clearAllMarkers = function() {
+    	aMap.clearMap();
+    }
+
+    // 被点击的poi 在地图上变红，前一个被点击的变为默认颜色
+    Map.prototype.fastMarkChange = function(index) {
+        this.updateMarkerWithIndex(this.selectedRedMarkerIndex, this.defaultMarkerImageUrl);
+        this.updateMarkerWithIndex(index, this.redMarkerImageUrl);
+        this.selectedRedMarkerIndex = index;
+
+        // 重设地图中心
+        this.resetMapCenter(this.markers[index].getPosition());
+    }
+
+    //  批量结果创建marker
+    Map.prototype.createMarkers = function(pois) {
+    	this.markers = [];
+    	this.selectedRedMarkerIndex = 0;
+        for (var i = 0; i < pois.length; i += 1) { 
+        	this.markers.push(this.createMarker(pois[i],i));
+        }
+    }
+
+    // 更新Marker的外观
+    Map.prototype.updateMarkerWithIndex = function (index,url) {
+    	this.updateMarker(this.markers[index], url);
+    }
+
+    // 更新markdown外观
+    Map.prototype.updateMarker = function(marker,url) {
+    	// 自定义点标记内容
+        var markerContent = document.createElement("div");
+
+    	// 点标记中的图标
+        var markerImg = document.createElement("img");
+        markerImg.className = "markerlnglat";
+        markerImg.src = url;
+        markerContent.appendChild(markerImg); 
+
+        marker.setContent(markerContent); //更新点标记内容 
+    }
+
+    //  创建单个marker
+    Map.prototype.createMarker = function(poi,i) {
+        var marker = new AMap.Marker({
+            position: [poi.location.lng, poi.location.lat],
+            title: poi.name,
+            extData: i
+        });
+        marker.setMap(aMap);
+
+        // 绑定 click 事件
+        AMap.event.addListener(marker, 'click', this.onMarkerClick.bind(this));
+
+        return marker;
+    }
+
+    // marker被点击
+    Map.prototype.onMarkerClick = function(obj) {
+    	var index = obj.target.getExtData();
+    	this.fastMarkChange(index);
+    	viewModel.selectedPoiIndex(index);
+    	aMap.setCenter(obj.lnglat)
+    }
+
+    Map.prototype.createIcon = function(url) {
+    	var icon = new AMap.Icon({
+	        image : url,//24px*24px
+	        //icon可缺省，缺省时为默认的蓝色水滴图标，
+	        size : new AMap.Size(24,24)
+        });
+        return icon;
+    }
+
+    // 定位到当前位置（未用上）
+    Map.prototype.selfLocation = function() {
+        var toolBar,
+            customMarker = new AMap.Marker({
+                offset: new AMap.Pixel(-14, -34), //相对于基点的位置
+                icon: new AMap.Icon({ //复杂图标
+                    size: new AMap.Size(27, 36), //图标大小
+                    image: "http://webapi.amap.com/images/custom_a_j.png", //大图地址
+                    imageOffset: new AMap.Pixel(-28, 0) //相对于大图的取图位置
+                })
+            });
+
+        aMap.plugin(["AMap.ToolBar"], function() {
+            toolBar = new AMap.ToolBar({ locationMarker: customMarker }); //设置地位标记为自定义标记 
+        });
+
+        aMap.addControl(toolBar);
+        toolBar.doLocation();
+    } 
+
+     // 重设地图中心 
+    Map.prototype.resetMapCenter = function(obj) { 
+    	aMap.setCenter(obj)
+    }      
+
+    window.mapControl = new Map();
+})();
